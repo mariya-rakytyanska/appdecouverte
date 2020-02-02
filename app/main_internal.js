164beb29609474ce2efd6d5d16041571a5a55bea
Mariya <maryrakytyanska@gmail.com>
2020-02-02 11:46:35 +0100

    fixing map zoom

--- app/main_internal.js
+++ app/main_internal.js
@@ -8,7 +8,9 @@ var map = L.map('map', {
     minZoom: 17.55,
     maxZoom: 17.55,
     dragging: false,
-    maxBounds: imageBounds
+    maxBounds: imageBounds,
+    maxBoundsViscosity: 1.0,
+    touchZoom: false
 });
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
