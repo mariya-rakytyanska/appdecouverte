f8022229cd41997ea9b516138fe8736fdefba083
Mariya <maryrakytyanska@gmail.com>
2020-02-05 09:25:16 +0100

    remove 3rd floor

--- app/main_internal.js
+++ app/main_internal.js
@@ -55,21 +55,6 @@ var plan_b10etage2 = L.map('plan_b10etage2', {
 var image = L.imageOverlay('img/b10etage2.png', bounds2).addTo(plan_b10etage2);
 plan_b10etage2.fitBounds(bounds2);
 
-var bounds13 = [[0,0], [462,548]];
-var plan_b10etage3 = L.map('plan_b10etage3', {
-    crs: L.CRS.Simple,
-    attributionControl: false,
-    minZoom: 0,
-    maxZoom: 0,
-    // dragging: false,
-    zoomControl:false,
-    // maxBounds: bounds2,
-    // maxBoundsViscosity: 1.0,
-    touchZoom: false
-    
-});
-var image = L.imageOverlay('img/b10etage2.png', bounds13).addTo(plan_b10etage3);
-plan_b10etage3.fitBounds(bounds13);
 
 
 var bounds3 = [[0,0], [459,548]];
